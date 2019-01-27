e0981fe78843acdef9c3b609aeefe02b4eab554a
ba <ba@barts-MacBook-Pro-2.local>
2019-01-27 23:23:44 +0100

    addds saving

--- project.clj
+++ project.clj
@@ -10,10 +10,12 @@
                  ;; [http-kit "2.3.0"] ; Add to your project.clj.
                  [http-kit "2.4.0-alpha2"]
                  [clj-http "3.9.1"]
+                 [environ "1.1.0"]
                  [co.paralleluniverse/quasar-core "0.7.9"]
                  [co.paralleluniverse/pulsar "0.7.9"]
  [lambdaisland/deep-diff "0.0-25"]]
   :plugins [[refactor-nrepl "2.4.0"]
+            [lein-environ "1.1.0"]
             [cider/cider-nrepl "0.18.0"]]
   :jvm-opts ["-Dhttps.protocols=TLSv1.2,TLSv1.1"]
   :main  clj-tests.core)

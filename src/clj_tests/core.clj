0884ba9574df377e86d6a75a48399a3a6576c18b
ba <ba@barts-MacBook-Pro-2.local>
2019-01-27 23:22:57 +0100

    addds saving

--- src/clj_tests/core.clj
+++ src/clj_tests/core.clj
@@ -1,15 +1,16 @@
 (ns clj-tests.core
   ;;(:gen-class)
   (:require [clj-http.client :as http]
-            [environ.core :refer [env] ]) )
+            [environ.core :refer [env] ] ) )
 
 (def url "https://jsonplaceholder.typicode.com/posts")
 
+(defn delete-files-rec [fname]
+  (doseq [f (reverse (file-seq (clojure.java.io/file fname)))]
+    (clojure.java.io/delete-file f)))
 
 (def run-mode
   (env :run-mode))
-
-
 
 (def id-list (range 1 10))
 
@@ -18,32 +19,33 @@
 
 (defn save-snaphost[id content]
   (do
-    (println "aaa")
-    (with-open [w (clojure.java.io/writer (str "post_" id ".json") ) ]
+    (with-open [w (clojure.java.io/writer (str "./" save-dir "/post_" id ".json") ) ]
       (.write w content))
       (print (str "saving for id: " id ) )
         #{:done } ) )
- 
 
-;; (defn get-and-save-snaphsot[id]
-;;   (do (-> (get-snapshoot id) (:body) ((partial save-snaphost id) ) ))
-;;   #{:done_saved})
+(defn get-and-save-snaphsot[id]
+  (do (-> (get-snapshoot id) (:body) ((partial save-snaphost id) ) ) )
+  #{:done_saved})
 
-;;(def saving-agents (map (fn[id]  (send-off (agent #{}) (fn [status id] (get-and-save-snaphsot id)) id ) )  id-list )
-;;(def save-snapshot-agents[]  )
+(defn  run-saving-agents [] (map (fn[id] (send-off
+                                 (agent #{} )
+                                 (fn [status id] (get-and-save-snaphsot id)) id ) )
+                        id-list ) )
 
 
-;;(apply await-for saving-agents)
 
 (def save-dir (cond
                  (= run-mode "pre") "./pre"
                  (= run-mode "post") "./post"))
-(defn test[]
-  (do (println "a")
-      (with-open [w (clojure.java.io/writer "test.json")]
-        (.write w "ole"))
-      #{:done}))
+
+(defn ensure-clean-save-dir[]
+  (do (if (.exists (java.io.File. save-dir))
+        (delete-files-rec save-dir ) )
+      (.mkdir (java.io.File. save-dir) ) ) )
 
 (defn -main[& args]
-    (print (str "save to dir: " save-dir)))
+  (do (ensure-clean-save-dir)
+      (println (str "save to dir: " save-dir ) )
+      (let [sv-agents (run-saving-agents)] ) ) )
 
